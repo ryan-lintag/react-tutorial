f23e299dddfd961071e6f6279125be1065e5cbbf
Ryan Lintag <lintag.ryan@gmail.com>
2019-11-14 00:14:18 +0800

    Updates

---
+++ precache-manifest.d60fde1201b71c987667b8d9bca678a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b16dccbe2545bcb774fdba856bf141",
+    "url": "/ryan-lintag/react-tutorial/index.html"
+  },
+  {
+    "revision": "8d3a8899dcafadb30178",
+    "url": "/ryan-lintag/react-tutorial/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "34e30f6b7bd5ad11cf82",
+    "url": "/ryan-lintag/react-tutorial/static/js/2.9b6246ed.chunk.js"
+  },
+  {
+    "revision": "8d3a8899dcafadb30178",
+    "url": "/ryan-lintag/react-tutorial/static/js/main.f1c9aef3.chunk.js"
+  },
+  {
+    "revision": "178455cd722a75fcd812",
+    "url": "/ryan-lintag/react-tutorial/static/js/runtime-main.f4a5042b.js"
+  }
+]);
